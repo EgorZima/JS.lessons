6e46aca92e864671973eccd33ee920a64fcb523d
EgorZima <egorzima21@gmail.com>
2016-12-17 01:26:15 +0200

    added 2 lesson

---
+++ lesson 2/Задание 5/js/main.js
@@ -0,0 +1,7 @@
+var name = 'admin', 
+            text;
+
+var result = (name == 'admin') ? text = 'hi' :
+             (name == 'manager') ? text = 'Hello' :
+             (name == 'no login') ? text = 'no login' :
+             text = ' ';
