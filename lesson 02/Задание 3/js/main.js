9eff177b9b3cb77dbb7d644a195cffb499e32d41
EgorZima <egorzima21@gmail.com>
2017-01-21 02:18:43 +0200

    Fixed code

---
+++ lesson 02/Задание 3/js/main.js
@@ -0,0 +1,18 @@
+var login = prompt('введите логин', ' ');
+
+if(login == 'admin'){
+	var password = prompt('введите пароль', '');
+	if(password == 'passw0rd'){
+		alert('welcome home')
+	} else if (pass == null) {
+        alert( 'Canceled' );
+    } else {
+        alert( 'Wrong password' );
+    }
+    } else if(login == null){
+	alert('access denied')
+    } else{
+    	alert('Access denied')
+    }  
+
+
