065477d6dccc5ea2b8567f11a4529306c9fd8126
EgorZima <zimaegor21@gmail.com>
2017-02-01 00:03:06 +0200

    Improve a slider

--- lesson 19 slider/js/main.js
+++ lesson 19 slider/js/main.js
@@ -1,5 +1,5 @@
 function Slider(selector, options) {
-  
+  var self = this;
   //DOM Nodes
   var sliderNode = document.querySelector(selector);
       sliderImagesNode = sliderNode.querySelector('.images-wrap');
@@ -8,9 +8,45 @@ function Slider(selector, options) {
       paginationNode = sliderNode.querySelector('.slider-pagination');
 
   var currentSlideIndex = options.currentImg || 0;
-  var imagesCount = sliderImagesNode.children.lenght;
+  var imagesCount = sliderImagesNode.children.length;
   var slideSize = sliderImagesNode.offsetWidth;
   
- 
+  
+  this.__render = function() {
+     sliderImagesNode.style.marginLeft = -(currentSlideIndex * slideSize) + 'px'
+  }
+
+
+  this.prevSlide = function() {
+  	if(currentSlideIndex === 0 ) {
+  		currentSlideIndex = imagesCount - 1;
+  		return
+  	}
+    currentSlideIndex--;
+   } 
+
+  this.nextSlide = function() {
+  	if(currentSlideIndex === imagesCount - 1) {
+  		currentSlideIndex = 0;
+  		return
+  	}
+    currentSlideIndex++;    
+  }
+
+  
+
+  prevArrow.onclick = function() {
+  	self.prevSlide()
+  	self.__render()
+  }
+
+  nextArrow.onclick = function() {
+  	self.nextSlide();
+  	self.__render()
+  }
+
+
+  
+  this.__render()
 }
 
