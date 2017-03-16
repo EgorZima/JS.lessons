474bc5d83fe7dc29e52837eedc99ff5f6c7367e3
EgorZima <zimaegor21@gmail.com>
2017-03-16 23:06:25 +0200

    Fixed gulp's errors

--- lesson 24 exam work/gulpfile.js
+++ lesson 24 exam work/gulpfile.js
@@ -4,30 +4,36 @@ let gulp = require('gulp'),
     concat = require('gulp-concat'),
     imagemin = require('gulp-imagemin'),
     del = require('del');
+    uglify = require('gulp-uglify');
 
 gulp.task('default', () => {
 	return del.sync('dist')
 })
 
 gulp.task('build', () => {
-	let buildCss = gulp.src('css/*.css')
+	let buildCss = gulp.src('app/css/*.css')
 	.pipe(cleanCSS({compatibility: 'ie8'}))
 	.pipe(rename("main.min.css"))
 	.pipe(gulp.dest('dist/css'));
 
-    let buildHtml = gulp.src('*.html')
+    let buildHtml = gulp.src('app/*.html')
     .pipe(gulp.dest('dist/'));
 
+    
+    let buildScripts = gulp.src('app/js/*.js')
+    .pipe(concat('min.js'))
+    .pipe(gulp.dest('dist/js'));
 
-    let buildImages = gulp.src('img/*')
+
+    let buildImages = gulp.src('app/img/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images'));
 
-    let buildImagesMasonry = gulp.src('img/masonry/*')
+    let buildImagesMasonry = gulp.src('app/img/masonry/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images/masonry'));
 
-    let buildImagesSlider = gulp.src('img/slider/*')
+    let buildImagesSlider = gulp.src('app/simg/slider/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images/slider'));
 
