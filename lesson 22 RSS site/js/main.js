abef80a48fc9145efff0330db4aa7e33f9421e64
EgorZima <zimaegor21@gmail.com>
2017-03-20 15:27:50 +0200

    Added new news APIs

--- lesson 22 RSS site/js/main.js
+++ lesson 22 RSS site/js/main.js
@@ -2,55 +2,56 @@
    
    class RSSReader {
       
-      constructor() {
-       this.feedURL = 'http://node.dev.puzankov.com/rss/data';
+      constructor() {  
+       this.newsSource = '';
+       this.newsSource = this.getUrlHash();
+       this.feedURL =  `https://newsapi.org/v1/articles?source=${this.newsSource}&sortBy=top&apiKey=f52da78d1f6e4d7290151421b119af96`;
        this.articles = document.querySelector('.articles');  
        this.news = document.querySelector('.one-news');
        this.menu = document.querySelector('.menu');
-
        this.init();
       } 
 
       init() {
-        var self = this;
-
-        this.getFeed(this.getUrlHash());
-
+        let self = this;
+        this.getFeed();
         this.currentMenuItem();
         
         window.onhashchange = function(e) {
-          self.articles.innerHTML = ' ';
+          self.articles.innerHTML = '';
+          self.newsSource = self.getUrlHash();
+          self.feedURL = `https://newsapi.org/v1/articles?source=${self.newsSource}&sortBy=top&apiKey=f52da78d1f6e4d7290151421b119af96`;
           
-          self.getFeed(self.getUrlHash());
+          self.getFeed();
           self.currentMenuItem();
         };         
       }
-
+      
+      
       currentMenuItem() {
-        var links = this.menu.querySelectorAll('a');
+        let links = this.menu.querySelectorAll('a');
            
         links.forEach(function(item, index) {
           item.classList.remove('current-link')
         });
         
-        var currentLink = this.getUrlHash();
-        var elemMenu = 'menu-' + currentLink;
+        let currentLink = this.getUrlHash();
+        let elemMenu = 'menu-' + currentLink;
 
-        var currentElem = this.menu.querySelector('.' + elemMenu);
+        let currentElem = this.menu.querySelector('.' + elemMenu);
         currentElem.classList.add('current-link');
       }
       
       getUrlHash() {
-        var hash = window.location.hash || 'habr';
-        
-        return hash.replace('#', '');
+        let hash = window.location.hash;
+
+        return hash.replace('#', '');    
       }
 
-      getFeed(feedId) {
-        var self = this;
+      getFeed() {
+        let self = this;
           $.ajax({
             url: this.feedURL,
-            data: {kind: feedId},
             method: 'GET',
             dataType: 'json'
           })
@@ -62,18 +63,16 @@
 	    }
 
       showError() {
-        var error = document.querySelector('.error');
+        let error = document.querySelector('.error');
         error.classList.remove('error-dis');
       }
 
       onGetData(data) {
-        var error = document.querySelector('.error');
-        error.classList.add('error-dis');
-        this.renderFeed(data.items);
+        this.renderFeed(data.articles);
       }
 
       renderFeed(dataList) {
-       var self = this,
+       let self = this,
            listHTML = [];
 
        dataList.forEach( function(item) {
@@ -82,7 +81,7 @@
 
        })
       
-      var newList = listHTML.join('');
+      let newList = listHTML.join('');
       
       this.articles.innerHTML += newList;
 
@@ -90,25 +89,28 @@
     
 
      renderItem(item) {
-      var newItem = this.news.cloneNode(true);
+      let newItem = this.news.cloneNode(true);
       newItem.querySelector('article').classList.remove('article-tmpl');
       
-      var post = newItem.querySelector('.post-heading')
+      let post = newItem.querySelector('.post-heading')
       post.innerHTML= item.title;
 
-      var author = newItem.querySelector('.author');
+      let author = newItem.querySelector('.author');
       author.innerHTML = item.author;
-  
-      
-      var date = newItem.querySelector('.date');
-      date.innerHTML = this.convertDate(item.pubDate);
+
+
+      let publishDate = newItem.querySelector('.date');
+      publishDate.innerHTML = this.convertDate(item.publishedAt);
       
 
-      var description = newItem.querySelector('.description');
-      description.innerHTML = item.summary;
+      let description = newItem.querySelector('.description');
+      description.innerHTML = item.description;
       
-      var readMore = newItem.querySelector('.read-more');
-      readMore.href = item.guid;
+      let readMore = newItem.querySelector('.read-more');
+      readMore.href = item.url;
+
+      let image = newItem.querySelector('.news-image') ;
+      image.src = item.urlToImage;
      
       
       if (newItem.querySelector('table') ) {
@@ -118,8 +120,8 @@
      }
 
 
-     convertDate(date) {
-        var date = new Date(date);
+     convertDate(dateId) {
+        let date = new Date(dateId);
          
         function month(date) {
           if (date.getMonth() < 10) {
@@ -128,7 +130,7 @@
           date.getMonth() + 1;
         }
 
-        var newDate = date.getDate() + '.'  +  month(date) + '.' + date.getFullYear();
+        let newDate = date.getDate() + '.'  +  month(date) + '.' + date.getFullYear();
               
         return newDate;
      }
